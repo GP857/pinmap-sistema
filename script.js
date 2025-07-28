410c561cc016773b40c5846db766ad96a7678eb4
GUSTAVO PINHEIRO <gp70ia@gmail.com>
2025-07-28 09:30:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function inicializarMapa() {
     // Criar grupo de marcadores com clustering
     marcadores = L.markerClusterGroup({
         chunkedLoading: true,
-        maxClusterRadius: 100,
+        maxClusterRadius: 500,
         spiderfyOnMaxZoom: true,
         showCoverageOnHover: false,
         zoomToBoundsOnClick: true,
